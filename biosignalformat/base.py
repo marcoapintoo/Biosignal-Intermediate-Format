20f298a3b93605b122c262226eee511ac5b6f4bf
Marco Antonio <marcoapintoo@hotmail.com>
2015-08-07 09:47:54 -0300

    Define conceptual structure: Experiment/Subject/Session/Channel

--- biosignalformat/base.py
+++ biosignalformat/base.py
@@ -23,7 +23,12 @@ class BaseFile(object):
 
     def readMetadata(self, provider, dirpath=[]):
         """Removes the metadata inside the archive provider, with a like-list directory path. """
-        return json.loads(provider.read(dirpath + [self.MetadataFileName]))
+        self.metadata = json.loads(provider.read(dirpath + [self.MetadataFileName]))
+        return self.metadata
+
+    @property
+    def pathname(self):
+        return []
 
 
 class Experiment(BaseFile):
@@ -60,3 +65,114 @@ class Experiment(BaseFile):
         """Remove a subject."""
         self.subjects.remove(subject)
         self.deletedSubjects.append(subject)
+
+class Subject(BaseFile):
+    """Represents a subject experiment in the sense of BIF."""
+    def __init__(self, metadata={}):
+        super(Experiment, self).__init__(metadata)
+        self.experiment = None
+        self.sessions = []
+        self.deletedSessions = []
+
+    def write(self, provider):
+        """Write the content inside the archive provide."""
+        super(Experiment, self).write(provider)
+        for session in self.sessions:
+            session.write(provider)
+        for session in self.deletedSession:
+            session.remove(provider)
+        self.deletedSession = []
+
+    def remove(self, provider):
+        """Removes the content inside the archive provide."""
+        super(Experiment, self).remove(provider)
+        for subject in self.session + self.deletedSession:
+            session.remove(provider)
+        self.deletedSession = []
+
+    def addSession(self, session):
+        """Add a session, recognizing it as a child XD."""
+        self.sessions.append(session)
+        session.subject = self
+
+    def removeSession(self, session):
+        """Remove a session."""
+        self.sessions.remove(session)
+        self.deletedSession.append(session)
+
+    @property
+    def pathname(self):
+        return self.experiment.pathname + ["SUBJECT-" + self.uniqueID]
+
+class Session(BaseFile):
+    """Represents a session experiment of a subject in the sense of BIF."""
+    def __init__(self, metadata={}):
+        super(Experiment, self).__init__(metadata)
+        self.subject = None
+        self.channelDatasets = []
+        self.deletedChannelDatasets = []
+
+    def write(self, provider):
+        """Write the content inside the archive provide."""
+        super(Experiment, self).write(provider)
+        for channelDataset in self.channelDatasets:
+            channelDataset.write(provider)
+        for channelDataset in self.channelDatasets:
+            channelDataset.remove(provider)
+        self.deletedChannelDatasets = []
+        provider.add(dirpath + [self.MetadataFileName], json.dumps(self.metadata, indent=4))
+
+    def remove(self, provider):
+        """Removes the content inside the archive provide."""
+        super(Experiment, self).remove(provider)
+        for channelDataset in self.channelDatasets + self.deletedChannelDatasets:
+            channelDataset.remove(provider)
+        self.deletedChannelDatasets = []
+
+    def addChannelDatasets(self, channelDataset):
+        """Add a channel dataset, recognizing it as a child XD."""
+        self.channelDatasets.append(channelDataset)
+        channelDataset.session = self
+
+    def removeChannelDatasets(self, channelDataset):
+        """Remove a session."""
+        self.channelDatasets.remove(channelDataset)
+        self.deletedChannelDatasets.append(channelDataset)
+
+    @property
+    def pathname(self):
+        return self.experiment.pathname + ["SESSION-" + self.uniqueID]
+
+
+class ChannelDataset(BaseFile):
+    RawDataFileName = "raw_data"
+    """Represents a session experiment of a subject in the sense of BIF.
+    Why use uniqueID instead of channel name as a ID reference?
+    Because in some experiments, the experimenter could have more than one data
+    over the same channel in the same session. So, to avoid create a new channel,
+    it would be better to assign a uniqueID reference. Obviously, this is an
+    akward situation. But, the file format need to be standard
+    """
+    def __init__(self, metadata={}):
+        super(Experiment, self).__init__(metadata)
+        self.session = None
+        self.rawData = []
+
+    def write(self, provider):
+        """Write the content inside the archive provide."""
+        super(Experiment, self).write(provider)
+        provider.add(self.pathname + [self.RawDataFileName], json.dumps(self.rawData))
+
+    def remove(self, provider):
+        """Removes the content inside the archive provide."""
+        super(Experiment, self).remove(provider)
+        provider.remove(self.pathname + [self.RawDataFileName])
+
+    def loadData(self, provider):
+        """Read the content from the archive provide."""
+        self.rawData = json.loads(provider.read(self.pathname + [self.RawDataFileName]))
+        return self.rawData
+
+    @property
+    def pathname(self):
+        return self.experiment.pathname + ["CHANNEL-" + self.uniqueID]
--- biosignalformat/test.py
+++ biosignalformat/test.py
@@ -16,6 +16,31 @@ class TestBaseObjects(unittest.TestCase):
         self.assertEqual(metadata["name"], "Exp!")
         self.assertEqual(metadata["description"], "blah!")
 
+    def test_MinimalStructure(self):
+        provider = ArchiveProvider("experiment002.7z")
+        experiment = Experiment({
+            "name": "Exp!",
+            "description": "blah!"
+        })
+        subject = Subject({
+            "name": "Subject001",
+            "description": "description-subject!"
+        })
+        session = Session({
+            "name": "Subject001-Session001",
+            "description": "description-subject-session!"
+        })
+        channelDataset = ChannelDataset({
+            "name": "AF8"
+        })
+        channelDataset.rawData = [1e-1222, 2.344, 3.14159265358979323846264338327950288419716939937510582097494459230781640629]
+        experiment.write(provider)
+        with self.assertRaises(Exception):
+            experiment.remove(provider)
+        metadata = experiment.readMetadata(provider)
+        self.assertEqual(metadata["name"], "Exp!")
+        self.assertEqual(metadata["description"], "blah!")
+
 def test_all():
     suite = unittest.TestLoader().loadTestsFromTestCase(TestBaseObjects)
     unittest.TextTestRunner(verbosity=2).run(suite)
